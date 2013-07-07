bfd83d0c5317d5a17e39eb882d339598ed02d97d
David Wursteisen <david.wursteisen+git@gmail.com>
2013-07-08 00:12:29 +0200

    suppression des tests inutiles

---
+++ test/GameTest.java
@@ -0,0 +1,12 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import play.test.*;
+
+public class GameTest extends UnitTest {
+
+    @Test
+    public void should_check_something() {
+        Assertions.assertThat(true).isTrue();
+    }
+
+}
