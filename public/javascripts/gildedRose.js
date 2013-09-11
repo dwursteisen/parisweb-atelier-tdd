17b850f67de6a2342e325d7d9e26d1826fdfb81f
David Wursteisen <david.wursteisen+github@gmail.com>
2013-09-11 14:02:41 +0200

    premier test

--- public/javascripts/gildedRose.js
+++ public/javascripts/gildedRose.js
@@ -41,6 +41,8 @@ function update_quality() {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
             items[i].sell_in = items[i].sell_in - 1;
         }
+
+
         if (items[i].sell_in < 0) {
             if (items[i].name != 'Aged Brie') {
                 if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
--- public/javascripts/tests.js
+++ public/javascripts/tests.js
@@ -1,5 +1,22 @@
 
 
-test( "hello test", function() {
-    ok( 1 == "1", "Passed!" );
+test( "doit mettre à jour tous les items", function() {
+    deepEqual(extract("quality").from(items), [20, 0, 7, 80, 20, 6]);
 });
+
+
+
+
+
+//get values instead of keys
+var extract = function (fieldName) {
+    var result = [];
+    return {
+        from: function(array) {
+            $.each(array, function (index, elt) {
+                result.push(elt[fieldName]);
+            });
+            return result;
+        }
+    }
+}
