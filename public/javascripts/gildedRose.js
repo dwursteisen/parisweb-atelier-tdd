f2f28c8933fdc264b7ffc60878941f9feef7ac9e
David Wursteisen <david.wursteisen+github@gmail.com>
2013-09-11 15:20:51 +0200

    Fin de refactoring

--- public/javascripts/gildedRose.js
+++ public/javascripts/gildedRose.js
@@ -1,7 +1,8 @@
-function Item(name, sell_in, quality) {
+function Item(name, sell_in, quality, update) {
     this.name = name;
     this.sell_in = sell_in;
     this.quality = quality;
+    this.update = update;
 }
 
 function GildedRose() {
@@ -10,61 +11,64 @@ function GildedRose() {
 }
 
 GildedRose.prototype.init = function () {
-    this.items.push(new Item('+5 Dexterity Vest', 10, 20));
-    this.items.push(new Item('Aged Brie', 2, 0));
-    this.items.push(new Item('Elixir of the Mongoose', 5, 7));
-    this.items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-    this.items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-    this.items.push(new Item('Conjured Mana Cake', 3, 6));
+    this.items.push(new Item('+5 Dexterity Vest', 10, 20, Item.prototype.update_by_default));
+    this.items.push(new Item('Aged Brie', 2, 0, Item.prototype.update_agedBrie));
+    this.items.push(new Item('Elixir of the Mongoose', 5, 7, Item.prototype.update_by_default));
+    this.items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80, Item.prototype.update_Sulfuras));
+    this.items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20, Item.prototype.update_BackstagePasses));
+    this.items.push(new Item('Conjured Mana Cake', 3, 6, Item.prototype.update_by_default));
 }
 
 
-GildedRose.prototype.update_quality = function() {
-    for (var i = 0; i < this.items.length; i++) {
-        if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-                if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                    this.items[i].quality = this.items[i].quality - 1
-                }
-            }
-        } else {
-            if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-                if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (this.items[i].sell_in < 11) {
-                        if (this.items[i].quality < 50) {
-                            this.items[i].quality = this.items[i].quality + 1
-                        }
-                    }
-                    if (this.items[i].sell_in < 6) {
-                        if (this.items[i].quality < 50) {
-                            this.items[i].quality = this.items[i].quality + 1
-                        }
-                    }
-                }
-            }
-        }
-        if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].sell_in = this.items[i].sell_in - 1;
-        }
+GildedRose.prototype.update_quality = function () {
+    $.each(this.items, function(index, item) { item.update() });
+}
 
 
-        if (this.items[i].sell_in < 0) {
-            if (this.items[i].name != 'Aged Brie') {
-                if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (this.items[i].quality > 0) {
-                        if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                            this.items[i].quality = this.items[i].quality - 1
-                        }
-                    }
-                } else {
-                    this.items[i].quality = this.items[i].quality - this.items[i].quality
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                }
+Item.prototype.update_agedBrie = function() {
+        if (this.quality < 50) {
+            this.quality = this.quality + 1
+        }
+        this.sell_in = this.sell_in - 1;
+        if (this.sell_in < 0) {
+            if (this.quality < 50) {
+                this.quality = this.quality + 1
             }
+        }
+}
+
+Item.prototype.update_BackstagePasses = function() {
+    if (this.quality < 50) {
+        this.quality = this.quality + 1
+        if (this.sell_in < 11) {
+            if (this.quality < 50) {
+                this.quality = this.quality + 1
+            }
+        }
+        if (this.sell_in < 6) {
+            if (this.quality < 50) {
+                this.quality = this.quality + 1
+            }
+        }
+    }
+    this.sell_in = this.sell_in - 1;
+    if (this.sell_in < 0) {
+        this.quality = this.quality - this.quality
+    }
+}
+
+Item.prototype.update_Sulfuras = function() {
+
+}
+
+Item.prototype.update_by_default = function() {
+    if (this.quality > 0) {
+        this.quality = this.quality - 1
+    }
+    this.sell_in = this.sell_in - 1;
+    if (this.sell_in < 0) {
+        if (this.quality > 0) {
+            this.quality = this.quality - 1
         }
     }
 }
--- public/javascripts/tests.js
+++ public/javascripts/tests.js
@@ -1,18 +1,45 @@
+test("doit mettre à jour tous les items", function () {
+    var inn = new GildedRose();
+    deepEqual(extract("quality").from(inn.items), [20, 0, 7, 80, 20, 6]);
+});
 
-
-test( "doit mettre à jour tous les items", function() {
-    deepEqual(extract("quality").from(items), [20, 0, 7, 80, 20, 6]);
+test("doit mettre à jour tous les items après plusieurs maj", function () {
+    var inn = new GildedRose();
+    inn.update_quality();
+    deepEqual(extract("quality").from(inn.items), [19, 1, 6, 80, 21, 5]);
 });
 
 
+test("doit mettre à jour tous les items après plein de maj", function () {
+    var inn = new GildedRose();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    inn.update_quality();
+    deepEqual(extract("quality").from(inn.items), [0,38,0,80,0,0]);
+});
 
 
-
-//get values instead of keys
 var extract = function (fieldName) {
     var result = [];
     return {
-        from: function(array) {
+        from: function (array) {
             $.each(array, function (index, elt) {
                 result.push(elt[fieldName]);
             });
