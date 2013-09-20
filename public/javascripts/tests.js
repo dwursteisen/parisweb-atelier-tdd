b4748f76cbe18d16ffcf329ea576eb19986a5373
David Wursteisen <david.wursteisen+git@gmail.com>
2013-09-20 16:12:00 +0200

    Ajout jQuery + méthode bien pratique d'extraction de donnée

--- public/javascripts/tests.js
+++ public/javascripts/tests.js
@@ -3,3 +3,21 @@
 test( "hello test", function() {
     ok( 1 == "1", "Passed!" );
 });
+
+
+
+
+
+
+// extract("nomChamp").from(arrayOfObject);
+var extract = function (fieldName) {
+    var result = [];
+    return {
+        from: function (array) {
+            $.each(array, function (index, elt) {
+                result.push(elt[fieldName]);
+            });
+            return result;
+        }
+    }
+}
